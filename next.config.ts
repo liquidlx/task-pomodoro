0b212de54112cae825ee17765c71c8fa906697cd
liquidlx <collatto99@gmail.com>
2025-06-11 15:57:17 -0300

    chore: fix build project

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/task-pomodoro",
+  assetPrefix: "/task-pomodoro",
 };
 
 export default nextConfig;
