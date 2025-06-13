93772f6532b4f935482422d28b651d7d382b0a31
Leonardo Collatto <leonardocollatto@MacBook-Pro-de-Leonardo.local>
2025-06-13 14:15:23 -0300

    chore: fix assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/task-pomodoro",
-  assetPrefix: "/task-pomodoro",
+  assetPrefix: "/task-pomodoro/",
   trailingSlash: true,
 };
 
